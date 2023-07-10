a4de7d02626d508c45862ae6b9cec4788fc8b32d
Louis Facun <louisfacun@gmail.com>
2023-07-11 03:22:37 +0800

    Update u2net_train.py

--- u2net_train.py
+++ u2net_train.py
@@ -45,16 +45,16 @@ def calculate_iou(mask1, mask2):
 bce_loss = nn.BCELoss(size_average=True)
 
 def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v):
-    loss0 = bce_loss(d0,labels_v)
-    loss1 = bce_loss(d1,labels_v)
-    loss2 = bce_loss(d2,labels_v)
-    loss3 = bce_loss(d3,labels_v)
-    loss4 = bce_loss(d4,labels_v)
-    loss5 = bce_loss(d5,labels_v)
-    loss6 = bce_loss(d6,labels_v)
-    
+    loss0 = F.binary_cross_entropy_with_logits(d0, labels_v)
+    loss1 = F.binary_cross_entropy_with_logits(d1, labels_v)
+    loss2 = F.binary_cross_entropy_with_logits(d2, labels_v)
+    loss3 = F.binary_cross_entropy_with_logits(d3, labels_v)
+    loss4 = F.binary_cross_entropy_with_logits(d4, labels_v)
+    loss5 = F.binary_cross_entropy_with_logits(d5, labels_v)
+    loss6 = F.binary_cross_entropy_with_logits(d6, labels_v)
+
     loss = loss0 + loss1 + loss2 + loss3 + loss4 + loss5 + loss6
-	#print("l0: %3f, l1: %3f, l2: %3f, l3: %3f, l4: %3f, l5: %3f, l6: %3f\n"%(loss0.data.item(),loss1.data.item(),loss2.data.item(),loss3.data.item(),loss4.data.item(),loss5.data.item(),loss6.data.item()))
+
     return loss0, loss
 
 
