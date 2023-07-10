56a9b6367e137a5d36ef2c41ae37f93aedadc953
Louis Facun <louisfacun@gmail.com>
2023-07-11 01:47:42 +0800

    Update u2net_train.py

--- u2net_train.py
+++ u2net_train.py
@@ -48,7 +48,7 @@ def multi_iou(d0, d1, d2, d3, d4, d5, d6, labels_v):
     pass
 
 def make_binary(d):
-    array = d.numpy()
+    array = d.cpu().numpy()
     binary = np.where(array > 0.6, 1,0).astype('float32')
     binary = binary.reshape((1, 1, 572, 572))
     tensor = torch.from_numpy(binary)
